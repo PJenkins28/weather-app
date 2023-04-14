4520618a4f672073248819191b650aa362ee3372
Paige Jenkins <paigemjenkins28@gmail.com>
2023-04-13 23:08:02 -0400

    implemented search feature

--- main.js
+++ main.js
@@ -2,7 +2,30 @@ let cityName = document.getElementById("city-name");
 let feelsLike = document.getElementById("feels-like");
 let currCard = document.querySelector(".current-info");
 let detailedInfo = document.querySelector(".detailed-info");
-let city = `sevilla`;
+const btn = document.getElementById("search-btn");
+const input = document.getElementById("search-item");
+const form = document.getElementById("form");
+let city = `atlanta`;
+window.addEventListener("DOMContentLoaded", (e) => {
+  getCity();
+  btn.addEventListener("click", (e) => {
+    city = input.value;
+    searchCity(input.value);
+    clearForm();
+  });
+  window.addEventListener("keydown", (e) => {
+    if (e.key === 13) {
+      e.preventDefault();
+      console.log("keydown event fired");
+      searchCity(input.value);
+    }
+  });
+});
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  searchCity(input.value);
+  clearForm();
+});
 
 async function getCity() {
   const response = await fetch(
@@ -16,6 +39,7 @@ async function getCity() {
   getCurrent(cityData);
 }
 function getCurrent(cityData) {
+  clearCurrent();
   // current temp
   getCurrentTemp(cityData);
   getCurrentIcon(cityData);
@@ -28,36 +52,49 @@ function getCurrent(cityData) {
 }
 function getCurrentTemp(cityData) {
   // Gets temperature in F for current day
-  let curTemp = document.createElement("div");
+  const curTemp = document.createElement("div");
   curTemp.classList.add("cur-temp");
   currCard.appendChild(curTemp);
   curTemp.textContent = `${cityData.current.temp_f}°`;
 }
 function getCurrentIcon(cityData) {
-  let icon = document.createElement("img");
+  const icon = document.createElement("img");
   icon.classList.add("icon");
   currCard.appendChild(icon);
   icon.src = cityData.current.condition.icon;
 }
 function getCurrentFeelsLike(cityData) {
-  let feelTemp = document.createElement("div");
+  const feelTemp = document.createElement("div");
   detailedInfo.appendChild(feelTemp);
   feelTemp.textContent = `Feels like ${cityData.current.feelslike_f}°F`;
 }
 function getCurrentVisibility(cityData) {
-  let visibility = document.createElement("div");
+  const visibility = document.createElement("div");
   detailedInfo.appendChild(visibility);
   visibility.textContent = `Visibility is ${cityData.current.vis_miles} miles`;
 }
 function getCurrentWindSpd(cityData) {
-  let windSpeed = document.createElement("div");
+  const windSpeed = document.createElement("div");
   detailedInfo.appendChild(windSpeed);
   windSpeed.textContent = `Wind ${cityData.current.wind_mph}mph ${cityData.current.wind_dir}`;
 }
 function getCurrentHumidity(cityData) {
-  let humidity = document.createElement("div");
+  const humidity = document.createElement("div");
   detailedInfo.appendChild(humidity);
   humidity.textContent = `Humidity ${cityData.current.humidity}%`;
 }
+function searchCity(search) {
+  city = `${search}`;
 
-getCity();
+  getCity();
+}
+function clearCurrent() {
+  currCard.innerHTML = "";
+  detailedInfo.innerHTML = "";
+  detailedInfo = document.createElement("div"); // create new detailedInfo element
+  detailedInfo.classList.add("detailed-info");
+  currCard.appendChild(detailedInfo);
+}
+function clearForm() {
+  input.value = "";
+}
